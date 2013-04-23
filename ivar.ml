05357ce58325e51930fa919eb3174d27c8c58504
Geoff Hulette <ghulette@gmail.com>
2013-04-23 08:08:57 -0700

    Update

--- ivar.ml
+++ ivar.ml
@@ -1,6 +1,6 @@
 type 'a t = {
   m : Mutex.t;
-  c : Condition.t;
+  full : Condition.t;
   value : 'a option ref
 }
 
@@ -10,28 +10,32 @@ let from_some = function
   | Some x -> x
   | None -> raise Not_found
 
+let empty ivar = 
+  match !(ivar.value) with
+  | None -> true
+  | Some _ -> false
+
+let full ivar = 
+  not (empty ivar)
+
 let create () = {
   m = Mutex.create ();
-  c = Condition.create ();
+  full = Condition.create ();
   value = ref None
 }
 
 let write ivar x =
   Mutex.lock ivar.m;
-  if !(ivar.value) <> None then begin
-    Mutex.unlock ivar.m;
-    raise IVar_full
-  end;
+  if full ivar then (Mutex.unlock ivar.m; raise IVar_full);
   ivar.value := Some x;
-  Condition.signal ivar.c;
+  Condition.broadcast ivar.full;
   Mutex.unlock ivar.m
 
 let read ivar =
   Mutex.lock ivar.m;
-  while !(ivar.value) == None do
-    Condition.wait ivar.c ivar.m
+  while empty ivar do
+    Condition.wait ivar.full ivar.m
   done;
-  let x = from_some !(ivar.value) in
-  Condition.signal ivar.c;
+  let contents = from_some !(ivar.value) in
   Mutex.unlock ivar.m;
-  x
+  contents
